6ceb67f907b613631c3249bb5a59b611d46e70c7
Marco Sampietro <marco.sampietro1574@gmail.com>
2025-02-26 13:10:42 +0000

    punto 6

--- sampietro-marco-fila-b/src/app/app.component.ts
+++ sampietro-marco-fila-b/src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { SecondoComponent } from './secondo/secondo.component';
 import { TerzoComponent } from './terzo/terzo.component';
 import { QuartoComponent } from './quarto/quarto.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, SecondoComponent, TerzoComponent, QuartoComponent],
+  imports: [SecondoComponent, TerzoComponent, QuartoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
